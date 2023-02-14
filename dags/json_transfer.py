50986586f320852b1578f42bf7a8809f9b11e6ed
ashishs1407 <shimpi.ashish.me2@gmail.com>
2023-02-15 03:08:03 +0530

    updated by ashish

--- dags/json_transfer.py
+++ dags/json_transfer.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from airflow.operators.email import EmailOperator
 from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from data_transform import read_json_file
 
 
@@ -24,6 +25,7 @@ with DAG('transfer_json_to_snowflake',
     ) as dag:
 
     #read_json_data = PythonOperator(task_id='read_json_data', python_callable=read_json_file)
+    start = EmptyOperator(task_id='Start')
 
     Create_table_in_sf = SnowflakeOperator(task_id='create_table',
     sql = 'create_table_json.sql',
@@ -41,6 +43,6 @@ with DAG('transfer_json_to_snowflake',
     sql = 'copy_into_table.sql',
     snowflake_conn_id='snowflake_conn')
 
-    
+    end = EmptyOperator(task_id='End')
 
-Create_table_in_sf >> transfer_json_int_stage >> copy_into_table
+start >> Create_table_in_sf >> transfer_json_int_stage >> copy_into_table >> end
